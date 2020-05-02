806db9d9c8a9b79db792ebb3b89d67f0945c07a6
root <root@iPhone>
2020-05-02 07:59:30 -0400

    order detail

--- src/app/components/orderdetail/orderdetail.component.ts
+++ src/app/components/orderdetail/orderdetail.component.ts
@@ -16,28 +16,74 @@ import { isError } from "util";
 export class OrderdetailComponent implements OnInit {
 
   constructor(
-    public _uw:UserWService,
-    private dataApi: DataApiService,
-    private location: Location,
-    private route:ActivatedRoute,
-    public router: Router,
-    private formBuilder: FormBuilder
+      public _uw:UserWService,
+      private dataApi: DataApiService,
+      private location: Location,
+      private route:ActivatedRoute,
+      public router: Router,
+      private formBuilder: FormBuilder
   	) { }
-    public orders:OrderInterface;
-    public order:OrderInterface;  
+  public orders:OrderInterface;
+  public order:OrderInterface;  
 
   getOrders(){
         this.dataApi
         .getOrders()
         .subscribe((orders: OrderInterface) => (this.orders=orders));
     }
+  checkSteep(){
+    if (this._uw.orderSelected.status=="new" ){
+      this._uw.new=true;
+
+    }
+    if (this._uw.orderSelected.status=="verifcar" ){
+      this._uw.pago=true;
+
+    }
+    if (this._uw.orderSelected.status=="proccess" ){
+      this._uw.proccess=true;
+
+    }
+    if (this._uw.orderSelected.status=="concretada" ){
+      this._uw.concretada=true;
+    }
+  }
+  setSteepOne(){
+    // this._uw.steepOne=true;
+    this._uw.new=true;
+    this._uw.pago=false;
+     this._uw.proccess=false;
+  }
+  setSteepTwo(){
+    this._uw.new=false;
+    this._uw.pago=true;
+    this._uw.proccess=false;
+  }
+  setSteepThree(){
+  // this._uw.steepOne=true;
+
+  // this._uw.verificar=true;
+  }
 
   ngOnInit() {
     this.getOrders();  	   
-    this.getDetails(this._uw.idSelected);
+    if (this._uw.idSelected != undefined ){
+      this.getDetails(this._uw.idSelected);
+    }
+    if (this._uw.orderSelected.steeps === undefined ){
+      this.router.navigate(['/orders']);
+    }
+    else {
+      if (this._uw.orderSelected != undefined && this._uw.orderSelected.steeps[2].steep==true){
+        this._uw.new=false;
+        this._uw.pago=false;
+        this._uw.proccess=true;
+      }
+    }
+    // this._uw.new=true;
   }
 
-    getDetails(id: string){
+  getDetails(id: string){
     this.dataApi.getOrderById(id).subscribe(order => (this._uw.orderSelected = order));
   }
 
